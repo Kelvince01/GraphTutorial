0599c4193b5d43150e0bc9f1d9c7b31592988672
Kelvince1 <themirkochip1@gmail.com>
2021-05-25 22:31:49 +0300

    Add project files.

---
+++ GraphTutorial/Graphs.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using WinRTXamlToolkit.Controls.DataVisualization.Charting;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace GraphTutorial
+{
+    public class Records
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+        public int Amount
+        {
+            get;
+            set;
+        }
+    }
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Graphs : Page
+    {
+        public Graphs()
+        {
+            this.InitializeComponent();
+            LoadChartContents();
+        }
+
+        private void LoadChartContents()
+        {
+            Random rand = new Random();
+            List<Records> records = new List<Records>();
+            records.Add(new Records()
+            {
+                Name = "Suresh",
+                Amount = rand.Next(0, 200)
+            });
+            records.Add(new Records()
+            {
+                Name = "C# Corner",
+                Amount = rand.Next(0, 200)
+            });
+            records.Add(new Records()
+            {
+                Name = "Sam",
+                Amount = rand.Next(0, 200)
+            });
+            records.Add(new Records()
+            {
+                Name = "Sri",
+                Amount = rand.Next(0, 200)
+            });
+            (PieChart.Series[0] as PieSeries).ItemsSource = records;
+            (ColumnChart.Series[0] as ColumnSeries).ItemsSource = records;
+            (lineChart.Series[0] as LineSeries).ItemsSource = records;
+        }
+    }
+}
